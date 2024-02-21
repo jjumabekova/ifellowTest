f04830563cdbf5b58cc2389d870ec24743cd12f3
Zhyldyz Zhumabekova <jyldyz0696@gmail.com>
2024-02-21 21:27:42 +0300

    init commit

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,3 +1,26 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class RunTest {
-    //its RunTest origin
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("beforeAll");
+    }
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("afterAll");
+    }
+    @Test
+    public void test1(){
+        System.out.println("1");
+        Assertions.assertEquals("AW", "AW");
+    }
+    @Test
+    public void test2(){
+        System.out.println("2");
+        Assertions.assertEquals("AW","A@W");
+    }
+
 }
